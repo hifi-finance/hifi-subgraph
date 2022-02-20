db8abb5923dc8619e77733fef0f5917d68f30e5c
scorpion9979 <ahmed.i.tawfeeq@protonmail.com>
2022-02-20 04:17:03 +0300

    refactor(helpers/constants): reorder

--- src/helpers/constants.template.ts
+++ src/helpers/constants.template.ts
@@ -1,5 +1,5 @@
 import { Address, BigDecimal } from "@graphprotocol/graph-ts";
 
-export const defaultHifiId = "1";
 export const chainlinkOperatorAddress = Address.fromString("{{contracts.chainlinkOperator.address}}");
+export const defaultHifiId = "1";
 export const zeroBd = BigDecimal.fromString("0");
